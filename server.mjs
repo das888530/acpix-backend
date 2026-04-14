257b36ec21f5134762c8641d04a9bc4f4e8ac897
unknown <das888530@gmai.com>
2026-04-14 20:24:36 +0530

    Initial commit

--- server.mjs
+++ server.mjs
@@ -350,6 +350,6 @@ app.delete("/api/videos/:id", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Backend API listening on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Backend API listening on http://0.0.0.0:${port}`);
 });
